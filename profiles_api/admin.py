160d7837e3be6a173447a17a046990ea09df5cfe
Jai <forthepeople4ever@gmail.com>
2022-06-17 18:01:58 +0530

    chn

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -1,9 +1,3 @@
 from django.contrib import admin
-from profiles_api import models
-
-admin.site.register(models.UserProfile)
-admin.site.register(models.ProfileFeedItem)
-
-
 
 # Register your models here.
--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6+10ilm$hzeqlrcq%uzomjjqbl_#j+-ba(nva(5x5n(c4eo%=d'
+SECRET_KEY = '&0c1)lx6-3_)u^2o#2th2)o8y&i@q!&%fo!+2&bp*wz8g7@%l('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
+    'rest_farmework',
     'rest_framework.authtoken',
     'profiles_api',
 ]
@@ -121,5 +121,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-AUTH_USER_MODEL = 'profiles_api.UserProfile'
